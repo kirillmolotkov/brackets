61c9cc347240486b53440687ac322e3e18d4ceb2
KirillMolotkov <balbes_goda@mail>
2022-07-22 17:21:38 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(string, config) {
+  const configObject = Object.fromEntries(config);
+  const stack = [];
+
+  for (let elem of string) {
+    const lastElementStack = stack[stack.length - 1];
+    if (configObject[lastElementStack] === elem && stack.length > 0) {
+      stack.pop();
+    } else {
+      stack.push(elem);
+    }
+  }
+  return stack.length === 0;
+};
